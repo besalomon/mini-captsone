516b7b1f1688b851d988a653fe07a2d26bc1b071
Salomon Beza <beza.salomon@gmail.com>
2017-01-23 16:45:29 +0200

    followed the naming conventions and changed my controllers and actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,12 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params[:id])
     render "product.html.erb"
   end
+
+  def new
+  end
+
+  def create
+    @var = Product.new({name: params[:name], price: params[:price], image: params[:image], description: params[:description]})
+    @var.save
+  end
 end
