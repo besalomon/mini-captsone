7706c056d3395ad9ee1b590d729275dea3afd067
Salomon Beza <beza.salomon@gmail.com>
2017-01-24 14:36:17 +0200

    applied rest and added a form for edit and delete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,4 +16,24 @@ class ProductsController < ApplicationController
     @var = Product.new({name: params[:name], price: params[:price], image: params[:image], description: params[:description]})
     @var.save
   end
+
+  def edit
+  @product = Product.find_by(id:params[:id])
+  render "edit.html.erb"
+  end
+
+  def update
+    @product = Product.find_by(id:params[:id])
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.image = params[:image]
+    @product.description = params[:description]
+    @product.save
+  end
+
+  def destroy
+    @item = Product.find_by(id:params[:id])
+    @item.delete
+    render "success.html.erb"
+  end
 end
