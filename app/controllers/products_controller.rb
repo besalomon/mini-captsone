3148f1d1d43f46d5485a6928eecedd361bd5d0a8
Salomon Beza <beza.salomon@gmail.com>
2017-02-02 11:13:13 +0200

    created show one image and show all

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,7 +19,6 @@ class ProductsController < ApplicationController
 	end
 
   def show
-    @image = Image.find_by(id: params[:id])
   if params[:id] == "random"
     @product = Product.all.sample
   else
